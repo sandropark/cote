9e11385b5e94a300bd80da225b2d9b01f5d81e8a
sandropark <birdob@gmail.com>
2024-04-03 13:48:23 +0900

    그리디-테스트 케이스 추가 및 로직 수정

--- greedy.py
+++ greedy.py
@@ -33,10 +33,34 @@ def greedy_2():
 
     first = arr[0]
     second = arr[1]
-    first_times = m//k*k
+    # first_times = m//k*k
+    first_times = m//(k+1)*k + m%(k+1)
+    assert first_times == 6
     second_times = m - first_times
+    assert second_times == 2
     result = first*first_times + second*second_times
-    
     assert result == 46
 
 greedy_2()
+
+def greedy_3():
+    arr = [2,4,5,4,6]
+    m = 8
+    k = 4
+
+    arr.sort(reverse=True)  # 내림차순 정렬 [6,5,4,4,2]
+
+    first = arr[0]
+    second = arr[1]
+    first_times = m//(k+1)*k + m%(k+1)  
+    # m에 k+1을 나눈 몫에 k를 곱하고 m에 k+1을 나눈 나머지를 더한다.
+    # m에 k+1을 나눈 몫 = 1 * k = 4
+    # m에 k+1을 나눈 나머지 = 8 % 5 = 3
+    # 둘을 더한 값 = 7 이게 가장 큰 수를 곱하는 수
+    assert first_times == 7
+    second_times = m - first_times
+    assert second_times == 1
+    result = first*first_times + second*second_times
+    assert result == 47
+
+greedy_3()
