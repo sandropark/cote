42fa4a06df08b62b32abcfad9c815017bc690593
sandropark <ticoba@naver.com>
2024-07-17 08:42:29 +0900

    DP - 금광 풀이 중

--- DP/p375_금광.py
+++ DP/p375_금광.py
@@ -14,30 +14,26 @@ class Test(unittest.TestCase):
 
 def solution(arr:list, n:int, m:int) -> int:
     temp_arr = [[0] for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            find_max(arr, temp_arr, i, j)
+    for col_idx in range(n):
+        for row_idx in range(m):
+            find_max(arr, temp_arr, col_idx, row_idx)
     return max(temp_arr[n-1])
 
 # 합이 가장 큰 조합을 찾아서 temp_arr를 갱신
-def find_max(arr, temp_arr, i, j):
-    pass
+def find_max(arr, temp_arr, col_idx, row_idx):
+    # 이전 결과 찾기
+    pre_col_idx = col_idx - 1
+    pre_row_idx = row_idx - 1
+    next_row_idx = row_idx + 1
+    
+    if pre_col_idx >= 0:
+        pass
+    
+    
+    if temp_arr[col_idx][row_idx] == 0:
+        temp_arr[col_idx][row_idx] = arr[col_idx][row_idx]
+        return
 
-
-
-# 바로 위 아래 대각선으로만 이동이 가능하다.
-# 현재 (1, 0) = 3
-# (0, 0), (0, 1)
-
-# (1, 1) = (0, 0), (0, 1), (0, 2)
-
-# pre_col_idx = i - 1
-
-
-# temp_e = temp_arr[i][j]
-# if temp_e == 0:
-#     temp_arr[i][j] = arr[i][j]
-        
 
 
 if __name__ == '__main__':
